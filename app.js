32386f04950df3cce42c05204217aee5650c0dd4
samandar <boboqulovsamandar087@gmail.com>
2025-01-16 21:15:56 +0500

    header yangilandi

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 
 
 window.addEventListener('scroll', () =>{
-const header =document.querySelector(".header")
-
-
+const header =document.querySelector(".header");
+const header_menu = document.querySelector('.header_menu');
 header.classList.toggle('active_bg' , window.scrollY > 50)
+
+header_menu.classList.toggle('active_li' , window.scrollY > 50)
+
 });
