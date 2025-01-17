a18175726b2797280fe8e58f252886996136e513
samandar <boboqulovsamandar087@gmail.com>
2025-01-17 11:30:31 +0500

    togirlandi

--- app.js
+++ app.js
@@ -3,8 +3,10 @@
 window.addEventListener('scroll', () =>{
 const header =document.querySelector(".header");
 const header_menu = document.querySelector('.header_menu');
+const header_search =document.querySelector(".header_search");
 header.classList.toggle('active_bg' , window.scrollY > 50)
 
 header_menu.classList.toggle('active_li' , window.scrollY > 50)
+header_search.classList.toggle('search' ,window.scrollY > 50)
 
 });
