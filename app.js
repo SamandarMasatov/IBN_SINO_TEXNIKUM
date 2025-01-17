c96c48ccb4366c5098519f66a39a2f4bbf3b8046
samandar <boboqulovsamandar087@gmail.com>
2025-01-17 18:35:29 +0500

    header yangilandi

--- app.js
+++ app.js
@@ -11,23 +11,25 @@ header_search.classList.toggle('search' ,window.scrollY > 50)
 
 });
 
-const telefon_menu = document.querySelectorAll('.telefon_menu');
-const telefon_select = document.querySelectorAll('.telefon_select');
-const telefon_cust = document.querySelectorAll('.telefon_cust');
-
-telefon_menu.forEach(item , () =>{
-    telefon_cust.classList.remov('telefon_active');
-    telefon_select.addEventListener('click' , () =>{
-        telefon_cust.classList.toggle('telefon_active');
-    })
-})
-
-// ================burger===========
 
 const burger = document.querySelector('.burger');
 
 const telefon = document.querySelector('.telefon')
 
 burger.addEventListener('click', () =>{
-    telefon.classList.toggle('active');
+    telefon.classList.add('active');
+    cover.classList.add('cover_active');
 })
+
+
+const telefon_close = document.querySelector('.telefon_close');
+const  cover = document.querySelector('.cover')
+telefon_close.addEventListener('click', () => {
+    telefon.classList.remove('active');
+    cover.classList.remove('cover_active');
+    
+})
+
+
+
+// ================burger===========
